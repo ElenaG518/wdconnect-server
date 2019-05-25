cf1ec0c3d53ad7919ada9158783c4ad7ed8a57e9
Elena Granados <patty518@yahoo.com>
2019-05-25 13:26:32 -0700

    specify routers and paths for each route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const usersRouter = require('./routes/api/users');
+const authRouter = require('./routes/api/auth');
+const profileRouter = require('./routes/api/profile');
+const postsRouter = require('./routes/api/posts');
 
 const app = express();
 
@@ -7,6 +11,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// define routes
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/posts', postsRouter);
 
 const PORT = process.env.PORT || 5000;
 

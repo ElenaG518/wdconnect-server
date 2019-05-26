bd272ca4e216a9e31185fecb6ad5cf0a28246dbd
Elena Granados <patty518@yahoo.com>
2019-05-25 19:49:04 -0700

    get user info from api/auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.json(user.serialize());
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;

24afa8bc31e9cd46a16538a6d0d18e18ea68df84
Elena Granados <patty518@yahoo.com>
2019-05-25 19:47:41 -0700

    add serialize method to user model

--- models/User.js
+++ models/User.js
@@ -25,4 +25,14 @@ const UserSchema = mongoose.Schema({
   }
 });
 
+UserSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    name: this.name || '',
+    email: this.email || '',
+    avatar: this.avatar,
+    date: this.date
+  };
+};
+
 module.exports = User = mongoose.model('User', UserSchema);

69ff85d53f5d3f85ec167d0f01bf1e90e5bd3666
Elena Granados <patty518@yahoo.com>
2019-05-26 21:28:38 -0700

    add PUT endpoint for adding blogpost section to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,20 +34,22 @@ router.get('/me', auth, async (req, res) => {
 
 router.post(
   '/',
-  auth,
   [
-    check('bio', 'Bio field is required')
-      .not()
-      .isEmpty(),
-    check('location', 'Please enter your location')
-      .not()
-      .isEmpty(),
-    check('skills', 'Please list your skills')
-      .not()
-      .isEmpty(),
-    check('hobbies', 'Please list your hobbies')
-      .not()
-      .isEmpty()
+    auth,
+    [
+      check('bio', 'Bio field is required')
+        .not()
+        .isEmpty(),
+      check('location', 'Please enter your location')
+        .not()
+        .isEmpty(),
+      check('skills', 'Please list your skills')
+        .not()
+        .isEmpty(),
+      check('hobbies', 'Please list your hobbies')
+        .not()
+        .isEmpty()
+    ]
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -156,6 +158,70 @@ router.get('/:id', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put(
+  '/blogpost',
+  [
+    auth,
+    [
+      check('title', 'Please enter the title of your post')
+        .not()
+        .isEmpty(),
+      check('content', 'Content is required')
+        .not()
+        .isEmpty(),
+      check('published', 'Published date is required')
+        .not()
+        .isEmpty(),
+      check(
+        'description',
+        'Please enter a short description of the content of your post'
+      )
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      content,
+      location,
+      published,
+      updated,
+      description
+    } = req.body;
+
+    const newBlog = {
+      title,
+      content,
+      location,
+      published,
+      updated,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.blogpost.unshift(newBlog);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route    DELETE api/profile
 // @desc     Delete profile, user & posts

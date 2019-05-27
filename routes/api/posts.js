9123f02f28565236ecc54edeee357b6df9ecb1df
Elena Granados <patty518@yahoo.com>
2019-05-27 15:02:44 -0700

    add GET endpoint for posts route to get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/Posts');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('content', 'Please add the content to your post')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id);
+
+      console.log('user ', user);
+
+      const newPost = {
+        user: req.user.id,
+        name: user.name,
+        username: user.username,
+        content: req.body.content,
+        avatar: user.avatar
+      };
+
+      const post = await Post.create(newPost);
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+  }
+);
 
 // @route    GET api/posts
-// @desc     Test route
-// @access   Public
+// @desc     Get all posts
+// @access   Private
 
-router.get('/', (req, res) => res.send('Posts route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
